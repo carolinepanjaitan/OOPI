2efb62047707816c902c0a7e8c856e4be5b10de1
carolinepanjaitan <carolinepanjaitan5@gmail.com>
2016-03-29 21:20:54 +0700

    project

---
+++ Perkenalan.java
@@ -0,0 +1,20 @@
+public class Perkenalan {
+
+    public static void main(String[] args) {
+        String nmA, nmB, tLhr, tglLhr, blnLhr, thnLhr;
+        nmA = "Caroline";
+        nmB = "Panjaitan";
+        tLhr = "Purwakarta";
+        tglLhr = "05";
+        blnLhr = "November";
+        thnLhr = "1992";
+        
+        System.out.println("Nama Lengkap :  " + nmA + " " + nmB);
+        System.out.println("Tempat Lahir : " + tLhr);
+        System.out.println("Tgl Lahir : " + tglLhr);
+        System.out.println("Bln Lahir : " + blnLhr);
+        System.out.println("Tahun Lahir : " + thnLhr);
+
+    }
+    
+}
