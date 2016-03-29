c8e03b12d32e794117f5eaac1f818c16efa5cc8d
carolinepanjaitan <carolinepanjaitan5@gmail.com>
2016-03-29 21:24:30 +0700

    project

---
+++ Pertambahan.java
@@ -0,0 +1,14 @@
+public class Pertambahan {
+
+    public static void main(String[] args) {
+        String li = "1";
+        String lu = "2";
+        String la = "3";
+        String le = "4";
+        String lo = "5";
+        
+        System.out.println(la + " " + li + " " + lu + " " + le + " " + lo);
+
+    }
+    
+}
